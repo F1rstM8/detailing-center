758fc56fe6c85c889165303e66c182c619e3874b
Maksym <praniknumberone@gmail.com>
2026-07-25 02:28:12 +0200

    refactor: move header styles to scss and add hover effects

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,51 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.scss'; // Подключаем наши стили
 
 const Header = () => {
   return (
-    <header
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "20px",
-        backgroundColor: "#242424",
-        color: "white",
-      }}
-    >
-      <div className="logo">
-        {/* Заглушка. Позже заменим на тег <img> с твоей фотографией */}
+    <header className="header">
+      
+      <div className="header__logo">
         <h2>Detailing Center</h2>
       </div>
-
-      <nav style={{ display: "flex", gap: "20px" }}>
-        <Link to="/" style={{ color: "white", textDecoration: "none" }}>
-          Услуги и цены
-        </Link>
-        <Link to="/blog" style={{ color: "white", textDecoration: "none" }}>
-          Блог
-        </Link>
-        <Link
-          to="/portfolio"
-          style={{ color: "white", textDecoration: "none" }}
-        >
-          Портфолио
-        </Link>
-        <Link to="/contacts" style={{ color: "white", textDecoration: "none" }}>
-          Контакты
-        </Link>
+      
+      <nav className="header__nav">
+        <Link to="/">Услуги и цены</Link>
+        <Link to="/blog">Блог</Link>
+        <Link to="/portfolio">Портфолио</Link>
+        <Link to="/contacts">Контакты</Link>
       </nav>
 
-      <div className="language-selector">
-        <select
-          defaultValue="ru"
-          style={{ padding: "5px", borderRadius: "4px" }}
-        >
+      <div className="header__lang">
+        <select defaultValue="ru">
           <option value="ru">Русский</option>
           <option value="en">English</option>
           <option value="pl">Polski</option>
         </select>
       </div>
+
     </header>
   );
 };
